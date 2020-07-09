0ed5acdcbc98948e90577a4622b10683aca5ddd7
LeoShestakov <leo.shestakov@hotmail.com>
2020-07-09 11:54:12 -0400

    dictionary

--- calc.py
+++ calc.py
@@ -1,2 +1,9 @@
-def add(x, y):
-    return x + y
+units_dictionary = {}
+units_dictionary['length'] = ['inch', 'foot', 'meter', 'centimeter']
+units_dictionary['volume'] = ['liter', 'gallon', 'pint', 'quart', 'cup']
+units_dictionary['time'] = ['second', 'minute', 'hour', 'day', 'week']
+units_dictionary['temperature'] = ['kelvin', 'fahrenheit', 'celsius']
+units_dictionary['mass'] = ['gram', 'pound', 'ounce']
+units_dictionary['data'] = ['MB', 'GB', 'KB', 'TB']
+units_dictionary['energy'] = ['joule', 'watt', 'BTU']
+# units_dictionary['gravity']
