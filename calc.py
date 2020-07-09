edafc99ab112c71fbfe2eecf8de8cde07c97c931
LeoShestakov <leo.shestakov@hotmail.com>
2020-07-09 14:28:08 -0400

    a

--- calc.py
+++ calc.py
@@ -1,13 +1,14 @@
 import data
+import custom_time
 
 units_dictionary = {}
-units_dictionary['length'] = ['inch', 'foot', 'meter', 'centimeter']
+#units_dictionary['length'] = ['inch', 'foot', 'meter', 'centimeter']
 units_dictionary['volume'] = ['liter', 'gallon', 'pint', 'quart', 'cup']
-units_dictionary['time'] = ['second', 'minute', 'hour', 'day', 'week']
-units_dictionary['temperature'] = ['kelvin', 'fahrenheit', 'celsius']
-units_dictionary['mass'] = ['gram', 'pound', 'ounce']
+units_dictionary['time'] = ['second', 'minute', 'hour', 'day']
+#units_dictionary['temperature'] = ['kelvin', 'fahrenheit', 'celsius']
+#units_dictionary['mass'] = ['gram', 'pound', 'ounce']
 units_dictionary['data'] = ['MB', 'GB', 'KB', 'TB']
-units_dictionary['energy'] = ['joule', 'watt', 'BTU']
+#units_dictionary['energy'] = ['joule', 'watt', 'BTU']
 # units_dictionary['gravity']
 
 # Choice[Category, From Unit, To Unit, Value]
@@ -20,7 +21,7 @@ def ask_input():
     choice.append(input("Enter your Category: "))
 
     print("CHOICES: " + str(units_dictionary[choice[0]]))
-    choice.append(input("Enter Unit your Converting to: "))
+    choice.append(input("Enter Unit you're Converting from: "))
 
     choice.append(input(f"Converting from {choice[1]} to : "))
     choice.append(int(input(f"How many {choice[1]}: ")))
@@ -30,6 +31,8 @@ def calc(choice):
     category = choice[0]
     if category == "data":
         return data.data(choice)
+    if category == "time":
+        return custom_time.timecalc(choice)
     else:
         print("ERROR")
         return
